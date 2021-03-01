0fba4a1d19b50aa8501c8ea0ae95822ec26a330d
TomsNikelis <nikelistoms@gmail.com>
2021-03-01 16:06:05 +0200

    1. lekcijas uzdevums pabeigts

---
+++ Assets/Scripts/VardaParadisana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour
+{
+    string teksts;
+    public GameObject ievadesLauks;
+    public GameObject tekstaAttelosana;
+
+    public void uzglabatTekstu()
+    {
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = "Sveiks" + teksts;
+    }
+}
